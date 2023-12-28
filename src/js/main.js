5c4a793905d8b9361628ab36c9cf6a3eebc3c0cb
chheangg <balldremz@gmail.com>
2023-12-28 10:27:31 -0800

    Refactor main loop for easier following

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,7 @@ import Game from './objects/game';
 
 let gameObject = {};
 let startCallback;
+const maxShips = 5;
 
 function resetGameObject() {
   Object.keys(gameObject).forEach((key) => delete gameObject[key]);
@@ -73,86 +74,63 @@ function setTurnName(playerName) {
   name.textContent = playerName;
 }
 
-// Loop that will be called on every cell click or startup
-// This will moves the game forward and does all the necessary calculations
-// First, check if gameObject is initialized. If not, initialize gameObject and page.
-// Game object must come from GameState global object.
-// Second, check if game has started, if not, goes into placement mode;
-// Third, if placement is done, all cells goes into play mode!
-export default function mainLoop(isMultiplayer, isInitialized, names, cb) {
-  if (gameObject.winner) {
-    resetGameObject();
-    restartGame();
-    startCallback();
-    return;
+function firstPlayerInit(mainLoopObj) {
+  if (!gameObject.playerOne.isBot) {
+    setTurnName(gameObject.playerOne.name);
+    placementMode(gameObject.playerOne, mainLoopObj);
+  } else {
+    // Initialize bot
+    initializeBot(gameObject.playerOne);
+    mainLoopObj(false, true);
   }
+}
 
-  if (!isInitialized) {
-    if (!startCallback) {
-      startCallback = cb;
-    }
-    const init = !isMultiplayer ? Player.singleplayerInit : Player.multiplayerInit;
-    gameObject = initializeObjects(isMultiplayer, init, names);
-  }
-
-  const maxShips = 5;
-
-  const numOfShipsPlayerOne = gameObject.playerOne.board.list.length;
-  const numOfShipsPlayerTwo = gameObject.playerTwo.board.list.length;
-
-  // If object is initialized, will goes into the placement mode for first player
-  // placement mode ends when max ships is reached
-  if (numOfShipsPlayerOne !== maxShips) {
-    if (!gameObject.playerOne.isBot) {
-      setTurnName(gameObject.playerOne.name);
-      placementMode(gameObject.playerOne, mainLoop);
-    } else {
-      // Initialize bot
-      initializeBot(gameObject.playerOne);
-      mainLoop(false, true);
-    }
-    return;
-  }
-
-  // Second placement mode for second player
-  // Signal to the first player to pass to the next player
-  if (numOfShipsPlayerTwo !== maxShips && numOfShipsPlayerOne === maxShips) {
-    // Depopulate left board events
-    if (!gameObject.playerTwo.isBot) {
-      setTurnName(gameObject.playerTwo.name);
-      if (numOfShipsPlayerTwo === 0 && !gameObject.playerOne.isBot) {
-        if (isMultiplayer) {
-          addPassDelay();
-        }
-      }
-      unloadBoard('left');
-      exitPlacementMode(gameObject.playerOne, mainLoop);
-      placementMode(gameObject.playerTwo, mainLoop);
-    } else {
-      // Initialize bot
-      initializeBot(gameObject.playerTwo);
-      mainLoop(false, true);
-    }
-    return;
-  }
-
-  const placementFinished = (gameObject.playerOne.board.list.length === maxShips)
-  && (gameObject.playerTwo.board.list.length === maxShips);
-
-  // Both player finishes placement
-  if (placementFinished && !gameObject.isStarted) {
-    gameObject.isStarted = true;
-    // Signal to the second player to pass the game to the first player
-    if (!gameObject.playerOne.isBot) {
-      unloadBoard('right');
+function secondPlayerInit(numOfShipsPlayerTwo, isMultiplayer, mainLoopObj) {
+  // Depopulate left board events
+  if (!gameObject.playerTwo.isBot) {
+    setTurnName(gameObject.playerTwo.name);
+    if (numOfShipsPlayerTwo === 0 && !gameObject.playerOne.isBot) {
       if (isMultiplayer) {
         addPassDelay();
       }
     }
-    exitPlacementMode(gameObject.playerTwo, mainLoop);
-    gameObject.isStarted = true;
+    unloadBoard('left');
+    exitPlacementMode(gameObject.playerOne, mainLoopObj);
+    placementMode(gameObject.playerTwo, mainLoopObj);
+  } else {
+    // Initialize bot
+    initializeBot(gameObject.playerTwo);
+    mainLoopObj(false, true);
   }
+}
 
+function initGame(isMultiplayer, names) {
+  const init = !isMultiplayer ? Player.singleplayerInit : Player.multiplayerInit;
+  gameObject = initializeObjects(isMultiplayer, init, names);
+}
+
+function handleGameWinner() {
+  resetGameObject();
+  restartGame();
+  startCallback();
+}
+
+function startGame(isMultiplayer, mainLoopObj) {
+  // Both player finishes placement
+  gameObject.isStarted = true;
+  // Signal to the second player to pass the game to the first player
+  if (!gameObject.playerOne.isBot) {
+    unloadBoard('right');
+    if (isMultiplayer) {
+      addPassDelay();
+    }
+  }
+  exitPlacementMode(gameObject.playerTwo, mainLoopObj);
+  gameObject.isStarted = true;
+}
+
+function runGame(isMultiplayer, mainLoopObj) {
+  // Run game and pass between each turn. Handle both player and both functionalities
   if (gameObject.isStarted) {
     // Signal to the player to pass the game to the opposite player.
     if (!gameObject.currentTurn().isBot) {
@@ -160,10 +138,60 @@ export default function mainLoop(isMultiplayer, isInitialized, names, cb) {
       if (isMultiplayer) {
         addPassDelay();
       }
-      attackMode(gameObject, mainLoop);
+      attackMode(gameObject, mainLoopObj);
     } else {
       // add bot attack
-      attackUtilities.botAttack(gameObject, mainLoop);
+      attackUtilities.botAttack(gameObject, mainLoopObj);
     }
   }
+}
+
+// Loop that will be called on every cell click or startup
+// This will moves the game forward and does all the necessary calculations
+// First, check if gameObject is initialized. If not, initialize gameObject and page.
+// Game object must come from GameState global object.
+// Second, check if game has started, if not, goes into placement mode;
+// Third, if placement is done, all cells goes into play mode!
+export default function mainLoop(isMultiplayer, isInitialized, names, cb) {
+  // Check if there's a game winner, if there is end the game
+  if (gameObject.winner) {
+    handleGameWinner();
+    return;
+  }
+
+  // Initialize game if it isn't already initialized
+  if (!isInitialized) {
+    if (!startCallback) {
+      startCallback = cb;
+    }
+    initGame(isMultiplayer, names);
+  }
+
+  const numOfShipsPlayerOne = gameObject.playerOne.board.list.length;
+  const numOfShipsPlayerTwo = gameObject.playerTwo.board.list.length;
+
+  // If object is initialized, will goes into the placement mode for first player
+  // placement mode ends when max ships is reached
+  if (numOfShipsPlayerOne !== maxShips) {
+    firstPlayerInit(mainLoop);
+    return;
+  }
+
+  // Second placement mode for second player
+  // Signal to the first player to pass to the next player
+  if (numOfShipsPlayerTwo !== maxShips && numOfShipsPlayerOne === maxShips) {
+    secondPlayerInit(numOfShipsPlayerTwo, isMultiplayer, mainLoop);
+    return;
+  }
+
+  const placementFinished = (gameObject.playerOne.board.list.length === maxShips)
+  && (gameObject.playerTwo.board.list.length === maxShips);
+
+  // Set board view correctly and pass to the first player to start the game
+  if (placementFinished && !gameObject.isStarted) {
+    startGame(isMultiplayer, mainLoop);
+  }
+
+  // run games once all the pre requisites are done above
+  runGame(isMultiplayer, mainLoop);
 }
